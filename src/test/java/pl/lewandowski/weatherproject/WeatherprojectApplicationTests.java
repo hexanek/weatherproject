c2cc08a2edb2b3cad325a01902b026854db36c2f
hexanek <hexanek@gmail.com>
2019-06-02 12:29:42 +0200

    Initial commit

---
+++ src/test/java/pl/lewandowski/weatherproject/WeatherprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.lewandowski.weatherproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
