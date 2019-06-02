07a4f2fcba6b3e9a911af7f3709ea3fa3dd6f515
hexanek <hexanek@gmail.com>
2019-06-02 12:37:57 +0200

    checking current weather

--- src/main/java/pl/lewandowski/weatherproject/Controller.java
+++ src/main/java/pl/lewandowski/weatherproject/Controller.java
@@ -1,8 +1,26 @@
 package pl.lewandowski.weatherproject;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class Controller {
 
+    @GetMapping("/pogoda/{country}/{city}")
+    public @ResponseBody Object getWeather(@PathVariable String country,
+                                           @PathVariable String city){
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        ResponseEntity<Object> response = restTemplate.getForEntity("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country +
+                        "&APPID=80730ceb55c51bebae397cfc46e03344",
+                Object.class);
+        return response;
+    }
 }
